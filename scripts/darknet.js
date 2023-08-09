d079da6081502ea33364588df611ae0c2fdfb2af
Josias Samael Olguin Rojas <130925399+jositsk@users.noreply.github.com>
2023-08-08 22:21:23 -0400

    Add files via upload

---
+++ scripts/darknet.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("myModal");
+    const passwordForm = document.getElementById("passwordForm");
+    const passwordInput = document.getElementById("passwordInput");
+    const mainContent = document.querySelector("main");
+  
+    // Show the modal
+    modal.style.display = "block";
+  
+    // Handle form submission
+    passwordForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const password = "3005"; // Replace with the actual password
+  
+      if (passwordInput.value === password) {
+        modal.style.display = "none";
+        // Show the main content
+        mainContent.style.display = "block";
+      } else {
+        alert("Incorrect password. Please try again.");
+      }
+    });
+  });
